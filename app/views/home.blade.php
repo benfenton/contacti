a9e61ee771c3e89b2a321a3e31d9d2160d67058d
Ben Fenton <benfenton@gmail.com>
2013-11-18 09:52:24 -0500

    change nav bar varialbe and recompile bootstrap

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -3,30 +3,22 @@
 <head>
   <meta charset="UTF-8">
   <title>CONTACT_i</title>
-  <link rel="stylesheet" type="text/css" href="js/libs/sass-bootstrap/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="js/libs/sass-bootstrap/dist/css/bootstrap.css">
   <!-- <script data-main="js/main" src="js/libs/requirejs/require.js"></script> -->
   <style>
-    #addContact { padding: 10px; margin: 125px 0 -50px 0; display: none; }
-    .navbar { margin-bottom: 100px; }
-    #contacts-table { margin-top: 100px; }
+    
+    .navbar {  }
+    #addContact { padding: 0 10px 35px 10px; display: none; }
+    #contacts-table { margin-top: -20px;}
+    button.new { margin: 10px; }
   </style>
 </head>
 <body>
-  <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+  <nav class="navbar navbar-default navbar-static-top" role="navigation">
     <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="#">CONTACT_I</a>
+      <a class="navbar-brand">CONTACT_I</a>
+      <button style="float:right;" class="new btn btn-primary">New Contact</button>
     </div>
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
-        <li><a class="new">New Contact</a></li>
-      </ul>
-    </div><!-- /.navbar-collapse -->
   </nav>
     <form id="addContact" class="module">
       <div>
@@ -45,21 +37,12 @@
         <label for="description">Description:</label>
         <textarea id="description"  class="form-control" name="description" ></textarea>
       </div>
+      <br />
       <div>
         <input type="submit" class="addContact btn btn-default" value="Add Contact"></div>
     </form>
-  <div id="contacts-table" class="table-responsive">
+    <div id="contacts-table" class="table-responsive">
     <table id="allContacts" class="table table-condensed">
-      <thead>
-        <tr>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-      </thead>
     </table>
   <div>
   <div id="editContact">
@@ -69,34 +52,47 @@
     <td><%= last_name     %></td>
     <td><%= email_address %></td>
     <td><%= description   %></td>
-    <td><a href="#contacts/<%= id %>/edit" class="edit">Edit</a></td>
+    <td><a href="#contacts/<%= id %>/edit" class="edit" data-toggle="modal" data-target="#myModal">Edit</a></td>
     <td><a href="#contacts/<%= id %>" class="delete">X</a></td>
   </script>
   <script id="editContactTemplate" type="text/template">
-    <header>
-      <h1>Edit Contact: <%= first_name %> <%= last_name %></h1>
-    </header>
-    <form id="editContact" class="">
+  <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button> 
+      <header>
+      <h2 class="modal-title" id="myModalLabel">Editing Contact:  <%= first_name %> <%= last_name %></h2>
+      </header>
+      </div>
+      <div class="modal-body">
+      <form id="editContact" class="">
       <div>
         <label for="edit_first_name">First Name:</label>
-        <input type="text" id="edit_first_name" name="edit_first_name" value="<%= first_name %>">
+        <input type="text" id="edit_first_name" class="form-control" name="edit_first_name" value="<%= first_name %>">
       </div>
       <div>
         <label for="edit_last_name">Last Name:</label>
-        <input type="text" id="edit_last_name" name="edit_last_name" value="<%= last_name %>">
+        <input type="text" id="edit_last_name" class="form-control" name="edit_last_name" value="<%= last_name %>">
       </div>
       <div>
         <label for="edit_email_address">Email Addres:</label>
-        <input type="text" id="edit_email_address" name="edit_email_address" value="<%= email_address %>">
+        <input type="text" id="edit_email_address" class="form-control" name="edit_email_address" value="<%= email_address %>">
       </div>
       <div>
         <label for="edit_description">Description:</label>
-        <textarea id="edit_description" name="edit_description"><%= description %></textarea>
+        <textarea id="edit_description"  class="form-control" name="edit_description"><%= description %></textarea>
       </div>
+      <br />
       <div>
-        <input type="submit" value="Save Changes"></div>
-        <button class="cancel" type="button">Cancel</button>
+        <button class="cancel btn btn-default" type="button" data-dismiss="modal">Cancel</button>
+        <input type="submit" class="btn btn-primary"  value="Save Changes"></input>
+      </div>
       </form>
+      </div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
   </script>
   <script src="http://code.jquery.com/jquery.js"></script>
   <script src="http://underscorejs.org/underscore.js"></script>
@@ -116,7 +112,7 @@
     });
   </script>
   <script>
-    $('a.new, .addContact').click(function() {
+    $('button.new, .addContact').click(function() {
       $( '#addContact' ).slideToggle( 'fast' );
     });
   </script>
